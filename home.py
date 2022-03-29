218998e6eaa17c4488e3c1aeb69f2a4d4d5d7d55
deptrajvjppro <99304464+deptrajvjppro@users.noreply.github.com>
2022-03-28 18:09:24 -0700

    Add files via upload

---
+++ home.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request,session,abort,flash
+import os
+import jinja2
+
+
+app_obj = Flask(__name__)
+app_obj.config['DEBUG'] =True
+
+@app_obj.route('/')
+def hello():
+    name = 'Lisa'
+    city_names = ["Paris","London","Rome","Tahiti"]
+    return f'''<html>
+    <head>
+        <title>Homework 3</title>
+    </head>
+    <body>
+        <h1>Welcome {name}</h1>
+        <p>
+            <a href = "https://www.google.com/">not google</a>
+                <ul>
+            <li>{city_names[0]}</li>
+            <li>{city_names[1]}</li>
+            <li>{city_names[2]}</li>
+            <li>{city_names[3]}</li>
+        </ul>
+        </p>
+    </body>
+</html>'''
+
+app_obj.run()
+
+
