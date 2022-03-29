bbae28e60df3765b304877c2c59a0c1965b4eb9a
deptrajvjppro <99304464+deptrajvjppro@users.noreply.github.com>
2022-03-28 20:32:19 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from config import Config
+import os
+SECRET_KEY = os.urandom(32)
 
 app_obj = Flask(__name__)
+
+app_obj.config['SECRET_KEY'] = SECRET_KEY
 
 from app import routes
