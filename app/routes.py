3a9bd491e912086c1860fdab4f203fabc37a7d05
deptrajvjppro <99304464+deptrajvjppro@users.noreply.github.com>
2022-03-28 20:37:26 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import app_obj
+from app import myobj
 from flask import Flask, render_template, request,session,flash
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -8,14 +8,13 @@ class SubmitForm(FlaskForm):
     cityname = StringField('City Name', validators=[DataRequired()])
     submitButton = SubmitField('Submit')
 
-@app_obj.route('/', methods = ['GET','POST'])
+@myobj.route('/', methods = ['GET','POST'])
 def home():
     name = 'Lisa'
     city_names = ["Paris","London","Rome","Tahiti"]
     form = SubmitForm()
     if form.validate_on_submit():
         flash(f'{form.cityname.data}')
-
     return render_template('home.html', name = name, city_names = city_names, form = form)
 
 
