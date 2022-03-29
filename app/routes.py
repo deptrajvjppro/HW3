2831da6638d03e75d8436e16a36b3f70abc92196
deptrajvjppro <99304464+deptrajvjppro@users.noreply.github.com>
2022-03-28 20:32:33 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,24 @@
 from app import app_obj
-from flask import Flask, render_template, request,session,abort,flash
-from app.submit import LoginForm
+from flask import Flask, render_template, request,session,flash
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
 
+class SubmitForm(FlaskForm):
+    cityname = StringField('City Name', validators=[DataRequired()])
+    submitButton = SubmitField('Submit')
 
-
-@app_obj.route('/',methods = ['GET','POST'])
-def hello():
-    form = LoginForm()
+@app_obj.route('/', methods = ['GET','POST'])
+def home():
     name = 'Lisa'
     city_names = ["Paris","London","Rome","Tahiti"]
+    form = SubmitForm()
     if form.validate_on_submit():
         flash(f'{form.cityname.data}')
-    return render_template('home.html',title ="Homework3", name = name, city_names=city_names,form = form)
+
+    return render_template('home.html', name = name, city_names = city_names, form = form)
 
 
-app_obj.run()
+
+
 
