61d35033c912696809c22e90fe0c35b3ea4e1c61
deptrajvjppro <99304464+deptrajvjppro@users.noreply.github.com>
2022-03-28 21:03:01 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
 
 
-import os
-SECRET_KEY = os.urandom(32)
+
 
 myobj = Flask(__name__)
 
-myobj.config['SECRET_KEY'] = SECRET_KEY
+myobj.config.from_mapping(SECRET_KEY = "asdcs")
 
 from app import routes
