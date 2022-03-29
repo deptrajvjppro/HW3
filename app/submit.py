bf2c6b9494bac55b398d6b3db8eaaa5e0b1c6852
deptrajvjppro <99304464+deptrajvjppro@users.noreply.github.com>
2022-03-28 20:32:03 -0700

    Delete submit.py

--- app/submit.py
+++
@@ -1,7 +0,0 @@
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-from flask_wtf import FlaskForm
-
-class SubmitForm(FlaskForm):
-    submitText = StringField('City Name', validators=[DataRequired()])
-    submitButton = SubmitField('Submit')
